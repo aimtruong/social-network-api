6139799c5ecaa2b20b0d32d20d518edb75cee88d
Aimee Truong <aimtruong@ucdavis.edu>
2022-04-21 17:18:36 -0700

    user model, controller, route added w/out thoughts info yet

---
+++ controllers/user-controller.js
@@ -0,0 +1,76 @@
+
+const { User } = require('../models');
+
+const userController = {
+    // GET all users
+    getAllUser(req, res){
+        User.find({})
+            //.populate({
+            //    path: 'thoughts'
+            //})
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // GET a user
+    getUserById({ params }, res){
+        User.findOne({ _id: params.id })
+            //.populate({
+            //    path: 'thoughts',
+            //    select: '-__v'
+            //})
+            .select('-__v')
+            .then(dbUserData => {
+                // if no user is found
+                if(!dbUserData){
+                    res.status(404).json({ message: 'No user found with this id' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // POST a user
+    createUser({ body }, res){
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
+
+    // PUT a user
+    updateUser({ params, body }, res){
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                // if no user is found
+                if(!dbUserData){
+                    res.status(404).json({ message: 'No user found with this id' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    // DELETE a user
+    deleteUser({ params }, res){
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                // if no user is found
+                if(!dbUserData){
+                    res.status(404).json({ message: 'No user found with this id'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+};
+
+module.exports = userController;
---
+++ models/User.js
@@ -0,0 +1,51 @@
+
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        validate: {
+            validator: function(v){
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: "Please enter a valid email"
+        },
+        required: [true, "Email required"]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+}
+);
+
+// get total count of thoughts on retrieval
+UserSchema.virtual('friendCount').get(function(){
+    return this.friends.reduce((total, friend) => total + friend.length + 1, 0);
+});
+
+// create User model
+const User = model('User', UserSchema);
+
+module.exports = User;
